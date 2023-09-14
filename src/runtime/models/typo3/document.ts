c348dd4680249a214fc5efb4b69588d89930204b
David Mellen <d.mellen@remind.de>
2023-09-14 09:40:48 +0200

    Add images to document model

--- src/runtime/models/typo3/document.ts
+++ src/runtime/models/typo3/document.ts
@@ -1,6 +1,9 @@
+import { Typo3 } from '@remindgmbh/nuxt-typo3/dist/runtime/models'
+
 export interface Document {
     title: string
     content: string
     link: string
+    images: Typo3.Asset[]
     [key: string]: any
 }
